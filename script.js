b42c9268ac1f1196a8d07b32ce7233a8f5b10206
Kas Eam <KassandraEam@gmail.com>
2022-06-25 09:25:28 -0500

    Completed Steps 1-7

--- script.js
+++ script.js
@@ -2,9 +2,32 @@ const tmdbKey = 'c3c5caf5fcc527355e8d7aaf5e4759dd';
 const tmdbBaseUrl = 'https://api.themoviedb.org/3';
 const playBtn = document.getElementById('playBtn');
 
-const getGenres = () => {
-  
+const getGenres = async () => {
+  const genreRequestEndpoint = '/genre/movie/list';
+//const variable = `?key=value` in this case key = api_key and value = tmdbkey
+  const requestParams = `?api_key = ${tmdbKey}}`
+  const urlToFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`;
+
+  try {
+
+  } catch (err) {
+    console.log(err)
+  };
 };
+
+/*
+step 6 is to turn getGenres into an asynchronous function. So all I should have to do is before the parantheses is add the async keyword.
+const myFunc = async () => {
+  // Function body here
+};
+ 
+myFunc();
+
+https://www.notion.so/kassandraeam/Async-Await-4663edd63b04466e933ee5d5e6c78b10#284d6efa5eff48bcaa1126c65312cbec
+
+*/
+// try/catch statement. Leave try block empty. IN catch block log caught errors to console.
+console.log(requestParams);
 
 const getMovies = () => {
   const selectedGenre = getSelectedGenre();
